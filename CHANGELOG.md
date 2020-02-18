1880aad579d51527c6897a504f23eda86866cbe3
Will Hall <willh@helecloud.com>
2020-02-18 14:36:19 +0000

    Update for newer version and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 Because I should write down what happens.
 
+## v0.1.5
+
+Separate to module and test deployment of multiple functions.
+
 ## v0.1.1
 
 - Nikos and Carlos release.
--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,11 @@ module "lambda_serverless_node10" {
   subdomain     = "v0-1-4"
   s3_bucket     = "wh-terraform-hello"
 }
+
+module "lambda_serverless_node10" {
+  source        = "./modules/lambda-serverless-node10"
+  function_name = "hello-serverless-v0.1.5"
+  s3_key        = "v0.1.5/hello.zip"
+  subdomain     = "v0-1-5"
+  s3_bucket     = "wh-terraform-hello"
+}
