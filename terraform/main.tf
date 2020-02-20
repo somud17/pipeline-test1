4f9121858db8c11ab9217ad626aa9cc5b4257f25
Will Hall <willh@helecloud.com>
2020-02-20 14:18:45 +0000

    Correct terraform syntax

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,9 +9,8 @@ module "lambda_serverless_v0_1_6" {
   function_name     = "hello-serverless-v0-1-6"
   describe_function = "Hello Serverless Terraform v0.1.6"
 
-  subdomain     = "v0-1-6"
-  domain        = "go.willhallonline.net"
-  # edge_cert_arn = ""
+  subdomain = "v0-1-6"
+  domain    = "go.willhallonline.net"
 
   s3_key    = "v0.1.6/hello.zip"
   s3_bucket = "wh-terraform-hello"
