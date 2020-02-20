de49547042c17c6834bf54254745b9d87a5d230d
Will Hall <willh@helecloud.com>
2020-02-20 14:46:59 +0000

    Add version v0.1.7

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,16 @@ module "lambda_serverless_v0_1_6" {
   s3_key    = "v0.1.6/hello.zip"
   s3_bucket = "wh-terraform-hello"
 }
+
+module "lambda_serverless_v0_1_7" {
+  source = "./modules/lambda-serverless-node10"
+
+  function_name     = "hello-serverless-v0-1-7"
+  describe_function = "Hello Serverless Terraform v0.1.7"
+
+  subdomain = "v0-1-7"
+  domain    = "go.willhallonline.net"
+
+  s3_key    = "v0.1.7/hello.zip"
+  s3_bucket = "wh-terraform-hello"
+}
